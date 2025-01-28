544a63c30d6899906c22b930dddd0f2ec5713740
Roman Karbivskyi <vt231_krp@student.ztu.edu.ua>
2025-01-28 12:23:07 +0200

    update: error handling, /sendall command parsing & loop. add: separate func checkUserRole,

--- server/src/bot/index.ts
+++ server/src/bot/index.ts
@@ -9,6 +9,24 @@ if (!BOT_TOKEN) throw new Error("BOT_TOKEN is undefined");
 
 export const bot = new TelegramBot(BOT_TOKEN);
 
+const ERROR_MESSAGES = {
+  userNotFound: "User not found",
+  accessDenied: "Access denied",
+};
+
+const WELCOME_MESSAGE = `
+🦉 Welcome to Owl Post!
+Tap "Open" below to start managing your temporary emails. 💌
+`;
+
+const checkAdminUser = async (chatId: number) => {
+  const existUser = await UserService.getUser(chatId.toString());
+  if (!existUser) throw new Error(ERROR_MESSAGES.userNotFound);
+  if (existUser.role !== UserRole.ADMIN)
+    throw new Error(ERROR_MESSAGES.accessDenied);
+  return existUser;
+};
+
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const msgText = msg.text;
@@ -21,18 +39,14 @@ bot.on("message", async (msg) => {
         tgId: chatId.toString(),
       } as HydratedDocument<IUser>);
 
-      await bot.sendMessage(
-        chatId,
-        `
-🦉 Welcome to Owl Post!
-Tap "Open" below to start managing your temporary emails. 💌
-    `,
-      );
+      await bot.sendMessage(chatId, WELCOME_MESSAGE);
     } catch (err) {
       console.log(err);
     }
   } else if (msgText == "/sendall") {
     try {
+      await checkAdminUser(chatId);
+
       await bot.sendMessage(
         chatId,
         `
@@ -41,51 +55,67 @@ Flags:
   * m - message;
   * p - photo (optional).
 Example: "/sendall -p <url> -m <your message>"
-    `,
+      `,
+      );
+    } catch (err) {
+      console.error(err);
+      await bot.sendMessage(chatId, err.message);
+    }
+  } else if (msgText.startsWith("/sendall")) {
+    try {
+      await bot.sendMessage(
+        chatId,
+        `
+        Type "/sendall -m <your message>"
+        Flags: 
+          * m - message;
+          * p - photo (optional).
+        Example: "/sendall -p <url> -m <your message>"
+      `,
       );
     } catch (err) {
       console.error(err);
     }
   } else if (msgText.startsWith("/sendall")) {
     try {
-      const existUser = await UserService.getUser(chatId.toString());
-      if (!existUser) {
-        await bot.sendMessage(chatId, "User not found");
-        return;
-      }
-
-      if (existUser.role != UserRole.ADMIN) {
-        await bot.sendMessage(chatId, "Access denied");
-        return;
-      }
+      await checkAdminUser(chatId);
     } catch (err) {
-      console.error(err);
+      await bot.sendMessage(chatId, err.message);
+      return;
     }
 
-    const isPhoto = msgText.includes("-p");
-    const photoUrl = msgText.split(" ")[2];
+    const parts = msgText.split(" ");
+    const isPhoto = parts.includes("-p");
+    const photoUrl = isPhoto ? parts[2] : null;
 
-    const [_, message] = msgText.split("-m ");
+    if (isPhoto && !photoUrl) {
+      await bot.sendMessage(chatId, "Please provide a valid photo URL.");
+      return;
+    }
+
+    const message = parts.includes("-m")
+      ? parts.slice(parts.indexOf("-m") + 1).join(" ")
+      : null;
+    if (!message) {
+      await bot.sendMessage(chatId, "Please provide a message.");
+      return;
+    }
 
     const users = await UserService.getAllUsers();
-
     let counter: number = 0;
 
     for (const user of users) {
       try {
-        if (isPhoto) {
+        if (isPhoto && photoUrl) {
           await bot.sendPhoto(parseInt(user.tgId), photoUrl, {
             parse_mode: "Markdown",
             caption: message,
           });
-
-          counter += 1;
-          return;
+        } else {
+          await bot.sendMessage(parseInt(user.tgId), message, {
+            parse_mode: "Markdown",
+          });
         }
-        await bot.sendMessage(parseInt(user.tgId), message, {
-          parse_mode: "Markdown",
-        });
-
         counter += 1;
       } catch (err) {
         console.error(err);
